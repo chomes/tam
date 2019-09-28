8c769e7a3688161a1d7bf6575d5cc66bd4dd2056
James Joseph (CHomes) <jaayjay@gmail.com>
2019-09-28 16:14:15 +0100

    starting the auth module will make into a class and import authentication through code.

--- mods/auth_checker.py
+++ mods/auth_checker.py
@@ -1,2 +1,12 @@
-import configparser
 import oauth2
+
+
+# Function to use oauth to run scripts
+def oauth_req(url, token_key, token_secret, http_method="GET", post_body=b"", http_headers=None):
+    consumer = oauth2.Consumer(key=con_key, secret=con_secret)
+    token = oauth2.Token(key=token_key, secret=token_secret)
+    client = oauth2.Client(consumer, token)
+    resp, content = client.request(url, method=http_method, body=post_body, headers=http_headers)
+    return content
+
+
